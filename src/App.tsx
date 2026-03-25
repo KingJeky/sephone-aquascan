ca57b567ed27f427745e73a3a259c021d09c96e5
KingJeky <zakyzahran06@gmail.com>
2026-03-25 21:38:58 +0700

    Add basename to BrowserRouter in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* TAMBAHKAN basename DI SINI */}
+      <BrowserRouter basename="/sephone-aquascan">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
