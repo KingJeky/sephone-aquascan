62a2c7c28831a76ee306591f3acf7d4f2d68e669
KingJeky <zakyzahran06@gmail.com>
2026-03-25 21:19:21 +0700

    Refactor vite.config.ts and set base path
    Updated Vite configuration to remove unused imports and set base path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  // Tambahkan baris ini! Sesuaikan dengan nama repo kamu
+  base: '/sephone-aquascan/', 
+  
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
   },
-}));
+})
